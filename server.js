814a67889849b3fc8787d30b49979f757ddb2bb5
Loli Lupiañez <Otsoas@users.noreply.github.com>
2021-08-19 17:03:31 +0200

    add dynamic GET route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 
+//Se ve en el navegador con 
 const mockUserData = [{
     name: 'Mark'
   },
@@ -8,6 +9,8 @@ const mockUserData = [{
     name: 'Jill'
   }
 ]
+
+//sin parametros, se pasa user mediante constante del json
 app.get('/users', function (req, res) {
   res.json({
     success: true,
@@ -15,7 +18,20 @@ app.get('/users', function (req, res) {
     users: mockUserData
   })
 });
-
+// Esto http://localhost:8000/users/mark  es yna GET route creo ????????
+//:id significa variable y se le llama asi req.params.id
+// el parametro de la variable se pone en el navegador 
+//se ve en el navegador con http://localhost:8000/users/mark
+app.get('/users/:id', function (req, res) {
+  //muestra en consola
+  console.log(req.params.id)
+  //muestra en navegador
+  res.json({
+    success: true,
+    message: 'got one user',
+    user: req.params.id
+  })
+})
 
 
 app.listen(8000, function () {
